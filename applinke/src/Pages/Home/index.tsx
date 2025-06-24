6375d9180fd62f5f73a0dd0c85abc55c39459c48
leoporto166 <leonardo.porto166@gmail.com>
2025-06-24 20:50:33 -0300

    Home, login

--- applinke/src/Pages/Home/index.tsx
+++ applinke/src/Pages/Home/index.tsx
@@ -1,12 +1,35 @@
+import { Social } from "../../components/Social"
+
+import { FaInstagram} from "react-icons/fa"
 
 export function Home(){
 
     return(
-        <div className="w-full h-screen bg-neutral-900">
-            <h1 
-            className="text-white text-3xl">
-                Pagina Home
-            </h1>
+        <div className=" flex w-full h-screen bg-linear-to-b to-black from-gray-600 items-center justify-start py-4 flex-col">
+            <h1 className="md:text-5xl text-4xl text-white mt-20 font-bold">Leonardo</h1>
+            <span className="text-white mt-4 text-xl">Veja meus links 👇</span>
+
+            <main className="flex flex-col w-11/12 max-width-xl mt-5 text-center">
+
+                <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer" >
+                    <a >
+                        <p className="text-base md:text-lg" >
+                            Projeto 1
+                        </p>
+                    </a>
+                </section>
+
+                <footer className="flex justify-center gap-3 my-4">
+                    <Social url="https://www.instagram.com/leoportozz/">
+                        <FaInstagram size={35} color="#FFF"></FaInstagram>
+                    </Social>
+                </footer>
+
+
+
+            </main>
+
+            
         </div>
     )
 }
--- applinke/src/Pages/Login/index.tsx
+++ applinke/src/Pages/Login/index.tsx
@@ -1,9 +1,15 @@
-
+import { Link } from "react-router-dom"
 
 export function Login(){
     return(
-        <div>
-            <h1>Pagina Login</h1>
+        <div className="flex flex-col items-center justify-center w-full h-screen bg-linear-to-b to-black from-gray-600">
+            <Link to={"/"}>
+                <h1 className="mt-11 text-white mb-7 font-bold text-4xl md: text-5xl">
+                    Dev
+                    <span className="bg-gradient-to-r from-green-500 to-green-900 bg-clip-text text-transparent">Link</span>
+                </h1>
+            </Link>
+
         </div>
     )
 }
