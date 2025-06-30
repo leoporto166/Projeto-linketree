b966a76fb6ce7256bd7eebd2831c17929ddbe80b
leoporto166 <leonardo.porto166@gmail.com>
2025-06-30 20:26:18 -0300

    Criação vercel.json

--- applinke/src/Pages/Cadastro/index.tsx
+++ applinke/src/Pages/Cadastro/index.tsx
@@ -43,7 +43,7 @@ export function Cadastro(){
     return(
         <div className="flex flex-col items-center justify-center w-full h-screen bg-linear-to-b to-black from-gray-600">
             <Link to={"/"}>
-                <h1 className="mt-11 text-white mb-7 font-bold text-4xl md: text-5xl">
+                <h1 className="mt-11 text-white mb-7 font-bold text-4xl md:text-5xl">
                     Dev
                     <span className="bg-gradient-to-r from-green-500 to-green-900 bg-clip-text text-transparent">Link</span>
                 </h1>
